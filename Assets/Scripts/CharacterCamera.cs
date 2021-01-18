7a30aa191e19cac70320e789d8484bb4f599f0e3
levent <saltlevent0@gmail.com>
2021-01-18 17:19:02 +0300

    kamera takibi eklendi

---
+++ Assets/Scripts/CharacterCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamera : MonoBehaviour
+{
+    public Camera characterCam;
+    public Vector3 cameraConstantPosition;
+
+    //Kameranın yumuşatmasının gecikme zamanı.
+    public float smooth = 0.2f;
+    //kameranın referans noktası.
+    private Vector3 _vel = Vector3.zero;
+    //Yumuşatılmış pozisyon.
+    Vector3 smoothedPosition;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        //Karakteri takip eden kamera hareketi yumuşatıldı
+        smoothedPosition = Vector3.SmoothDamp(characterCam.transform.position, new Vector3(transform.position.x + cameraConstantPosition.x, cameraConstantPosition.y, transform.position.z + cameraConstantPosition.z), ref _vel, smooth);
+
+        //Takip işlemi update şeklinde çalışmakta
+        characterCam.transform.position = smoothedPosition;
+    }
+}
