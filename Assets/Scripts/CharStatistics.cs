d99fea2c226578f9a2830c5e51e2380acf0f6e17
levent <saltlevent0@gmail.com>
2021-01-27 21:56:25 +0300

    devam

--- Assets/Scripts/CharStatistics.cs
+++ Assets/Scripts/CharStatistics.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharStatistics : MonoBehaviour
 {
     float health;
+    float exp;
 
     void Start()
     {
-        
+        //Veriler çekilemediyse oyunu tekrar başlatır.
+        if (PlayerPrefs.HasKey("name") && PlayerPrefs.HasKey("exp"))
+        {
+            SceneManager.LoadScene(0);
+        }
+                
     }
 
     // Update is called once per frame
