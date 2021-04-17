8e8d0339a09973c954d13b89a3238778c4c37ce0
levent <saltlevent0@gmail.com>
2021-04-17 13:05:09 +0300

    botlar ateş edebiliyor :+1:
    Yani eklenmesi gerken fonksiyonlar var elbet. görür görmez ateş ediyor.
    
    buraya da not düşeyim: karakterin ateş etme sisteminde değişiklik yapacağım. sol parmak hareket, sağ parmak ise nişan alma şeklinde.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,14 +7,50 @@ public class EnemyAI : MonoBehaviour
 {
     NavMeshAgent navMesh;
 
-    float time;
-    public float updatePerSecond = 1;
+    public LayerMask playerLayerMask;
 
+    float time;
+    float gunTime;
+    public float updatePerSecond = 2;
+
+    //gamecontroller sınıfı çağırıldı
     GameController gameController;
-    
+
+    //Ateş ediyor mu kontrolü
+    bool isShooting = false;
+
+    //Merminin objesi (model vs.)
+    public GameObject bulletObject;
+
+    //İki atış arası geçen süre.
+    float fireRate = .25f;
+    //Merminin sürati
+    float bulletSpeed = 50;
+    //Merminin gücü
+    float hitPoint = 5;
+    //Merminin yok olma süresi.
+    float destroyTime = 3;
+
+
+    //elinde tuttuğu silahın ID numarası.
+    public int startGun = 2;
+
     void Start()
     {
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
+
+        //silah verisini çekip tek seferlik silah veriliyor bota.
+        foreach (BulletInfo item in gameController.gunList.gun)
+        {
+            if (startGun == item.gunId)
+            {
+                fireRate = item.fireRate;
+                bulletSpeed = item.bulletSpeed;
+                hitPoint = item.hitPoint;
+
+                break;
+            }
+        }
 
         //Yapay zekanın başlangıç hedef ayarları
         navMesh = GetComponent<NavMeshAgent>();
@@ -27,13 +63,16 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        gunTime += Time.deltaTime;
+
         if (time > updatePerSecond)
         {
             navMesh.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
             time = 0;
         }
-    }
 
+        AimAndShoot(GameObject.FindGameObjectWithTag("Player").transform);
+    }
 
     void DropGun(int gunRandomRange)
     {
@@ -52,6 +91,34 @@ public class EnemyAI : MonoBehaviour
     {
         DropGun(3);
         Destroy(gameObject);
+    }
+
+
+    public void AimAndShoot(Transform target)
+    {
+        RaycastHit hit;
+
+        
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10, playerLayerMask))
+        {
+            if (gunTime>fireRate)
+            {
+                //Belirlenen konumda kurşunu oluşturur.
+                GameObject bullet = Instantiate(bulletObject, transform.position, transform.rotation);
+                bullet.GetComponent<BulletScript>()._bulletSpeed = bulletSpeed;
+                bullet.GetComponent<BulletScript>()._hitPoint = hitPoint;
+                bullet.tag = transform.tag + "Bullet";
+
+                gunTime = 0;
+            }
+            Debug.DrawRay(transform.position, transform.forward * 10, Color.green);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * 10,Color.red);
+        }
+
     }
 
 }
--- Assets/Scripts/FireScript.cs
+++ Assets/Scripts/FireScript.cs
@@ -93,7 +93,6 @@ public class FireScript : MonoBehaviour
 
     }
 
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Gun")
